367ed5145b1eae8e0b31787af40e5e8eb8cb3219
Ariel Villalona <44641804+ArielVillalona@users.noreply.github.com>
2020-03-06 19:51:06 -0400

    seccion 7

---
+++ Hola Mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//operadores
+// + suma
+// - resta
+// * multiplicacion
+// / divicion
+
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 + numero2;
+
+console.log("el resultado de la operacion es: "+operacion);
+
+//tipos de datos
+var numero_entero = 44;
+var cadena_texto = 'hola esto es "una cadena"';
+var verdadero_falso = false;
+
+var numero_falso = "33";
+Number(numero_falso);//convertir a entero la cadena "33"
+
+console.log(typeof numero_entero);
+console.log(typeof cadena_texto);
+console.log(typeof verdadero_falso);
