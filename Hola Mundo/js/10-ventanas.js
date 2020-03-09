e657de6a6454a2ce8e3afd80cb70e14f4c55622d
Ariel Villalona <avillalona@farmahorro.com.do>
2020-03-09 12:10:18 -0400

    seccion 10

---
+++ Hola Mundo/js/10-ventanas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//alerta
+alert("Primera Alerta que hago WOW!");
+
+//alertas de confirmacion
+var mi_resultado = confirm("Estas seguro que desea continuar?");
+console.log(mi_resultado);
+
+//ingreso de datos
+var mi_edad = prompt("Que edad tienes?",18);
+console.log(mi_edad);
