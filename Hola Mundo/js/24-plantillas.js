eb2ae6e09af000b5cb0c9eeb32697d9bf78ce98e
ArielVillalona <a.villalona.reyes@hotmail.com>
2021-08-28 11:59:00 -0400

    plantillas

--- Hola Mundo/js/24-plantillas.js
+++ Hola Mundo/js/24-plantillas.js
@@ -1,1 +1,18 @@
 'use strict'
+
+//plantilla de texto
+
+var nombre = prompt("Mete tu nombre");
+var apellido = prompt("Mete tus apellidos");
+
+var texto = "Mi nombre es: "+ nombre +"<br/> Y mis apellidos son: "+apellido;
+
+
+var texto2 = `
+    <h1>Hola que tal</h1>
+    <h3>Mi nombre es: ${nombre}</h3>
+    <h3>y mis apellidos son: ${apellido}</h3>
+
+`
+
+document.write(texto+texto2);
