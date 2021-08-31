61976f63eea314432ac8f04a0bb7efc2e0ea49b3
ArielVillalona <a.villalona.reyes@hotmail.com>
2021-08-30 22:37:49 -0400

    bom

---
+++ Hola Mundo/js/29-bom.js
@@ -0,0 +1,9 @@
+'use strict'
+
+//bom - browser object model
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+console.log(screen.width);
+console.log(screen.height);
+console.log(window.location);
