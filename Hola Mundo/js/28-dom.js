75c4d17f4733f803e7eff980bc6656ceeb0c2df0
ArielVillalona <a.villalona.reyes@hotmail.com>
2021-08-30 22:15:27 -0400

    dom

--- Hola Mundo/js/28-dom.js
+++ Hola Mundo/js/28-dom.js
@@ -5,10 +5,34 @@ function cambiaColor(color){
     caja.style.background = color;
 }
 
-var caja = document.getElementById("micaja");
-
+//var caja = document.getElementById("micaja");
+var caja = document.querySelector("#micaja");
 caja.innerHTML = "YA NO SOY UNA CAJA";
 caja.style.background = "red";
 caja.style.padding = "20px";
 caja.style.color = "white";
 console.log(caja);
+
+var todosLosDivs = document.getElementsByTagName('div');
+console.log(todosLosDivs);
+
+todosLosDivs[2].innerHTML = "Era el div 2";
+todosLosDivs[2].style.background = "green";
+todosLosDivs[2].style.padding = "20px";
+todosLosDivs[2].style.color = "white";
+
+for(var valor in todosLosDivs)
+{
+    var parrafo= document.createElement("p");
+    var texto = document.createTextNode(todosLosDivs[valor].textContent);
+    parrafo.appendChild(texto);
+    document.querySelector("#miseccion").appendChild(parrafo);
+}
+
+var divsRojos = document.getElementsByClassName("rojo");
+for(let valor in divsRojos){
+    if(divsRojos[valor].className=='rojo'){
+        divsRojos[valor].style.background = "red";
+    }
+}
+console.log(divsRojos);
