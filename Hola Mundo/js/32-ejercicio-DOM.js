ac7e6b5261bbee426b0bacaf17bd5350e6ca367c
Ariel Villalona <avillalona@farmatrix.com.do>
2021-09-01 08:58:31 -0400

    seccion 19

--- Hola Mundo/js/32-ejercicio-DOM.js
+++ Hola Mundo/js/32-ejercicio-DOM.js
@@ -6,29 +6,44 @@ window.addEventListener('load',()=>{
     box_dashed.style.display = "none";
 
     formulario.addEventListener("submit",()=>{
-        box_dashed.style.display = "block";
+        
         var nombre = document.querySelector("#nombre").value;
         var apellido = document.querySelector("#apellido").value;
         var edad = document.querySelector("#edad").value;
 
-        if(nombre.trim() === null || nombre.trim().lenght==0){
-            alert("no valido");
-            return false;
+        if(nombre.trim() === '' || nombre.trim().lenght==0){
+            document.querySelector("#error_nombre").innerHTML = "NOMBRE NO VALIDO";
         }
-        if(apellido.trim() === null || apellido.trim().lenght==0){
-            alert("no valido");
-            return false;
+        else{
+            document.querySelector("#error_nombre").innerHTML = "";
         }
-        if(isNaN(edad) || edad==0){
-            alert("no valido");
-            return false;
-        }
-        var datos = [nombre,apellido,edad];
 
+        if(apellido.trim() === '' || apellido.trim().lenght==0){
+            document.querySelector("#error_apellido").innerHTML = "APELLIDO NO VALIDO";
+        }
+        else{
+            document.querySelector("#error_apellido").innerHTML = "";
+        }
+
+        if(isNaN(edad) || edad==0){
+            document.querySelector("#error_edad").innerHTML = "EDAD NO VALIDA";
+        }
+        else{
+            document.querySelector("#error_edad").innerHTML = "";
+        }
+
+        if(nombre==='' || apellido === '' || edad==0)
+        {
+            return false;
+        }
+
+        var datos = [nombre,apellido,edad];
         for(var index in datos){
             var parrafo = document.createElement("p");
             parrafo.append(datos[index]);
             box_dashed.append(parrafo);
         }
+
+        box_dashed.style.display = "block";
     })
 });
