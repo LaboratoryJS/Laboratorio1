b64a9e0a264eeb0d38972225ea08565d8b1b68cd
Ariel Villalona <avillalona@farmatrix.com.do>
2021-09-02 17:53:01 -0400

    directivas

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -15,7 +15,7 @@ export class ZapatillasComponent implements OnInit{
     constructor() {
         this.Zapatillas = [
             new Zapatilla('Rebook air','rebook','Azul',1000,true),
-            new Zapatilla('Nike air','Nike','Griz',1000,true),
+            new Zapatilla('Nike air','Nike','Griz',1500,true),
             new Zapatilla('Jordan air','Jordan','Azul',1000,false),
             new Zapatilla('Adidas air','Adidas','Negras',1000,true),
             new Zapatilla('Sd air','rebook','Sd',1000,true),
