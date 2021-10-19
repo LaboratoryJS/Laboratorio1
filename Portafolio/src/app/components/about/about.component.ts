70646291ad4fa6096e1de3437cb0af14a2ab3090
Ariel Villalona <avillalona@farmatrix.com.do>
2021-10-19 13:00:59 -0400

    maqueting

--- Portafolio/src/app/components/about/about.component.ts
+++ Portafolio/src/app/components/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title:string;
+  public subtitle:string;
+  public email:string;
 
-  constructor() { }
+  constructor() {
+    this.title = "Ariel Villalona";
+    this.subtitle = "Desarrollador fullstack Web y Desktop";
+    this.email = "a.villalona.reyes@hotmail.com";
+  }
 
   ngOnInit(): void {
   }
