739f9113764ccb049c9239eb32225241571b4235
Ariel Villalona <avillalona@farmatrix.com.do>
2021-10-19 11:56:05 -0400

    angular project

---
+++ Portafolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectComponent } from './components/project/project.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProjectComponent,
+    CreateComponent,
+    ContactComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
